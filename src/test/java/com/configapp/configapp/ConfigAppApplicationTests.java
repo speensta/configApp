11b0d4644d9ae97c48efbb89991e00ad97e131a9
speensta <47218450+speensta@users.noreply.github.com>
2022-07-21 23:26:26 +0900

    Initial commit

---
+++ src/test/java/com/configapp/configapp/ConfigAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.configapp.configapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootTest
+class ConfigAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
